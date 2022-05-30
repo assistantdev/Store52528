414208d2166b3ed37b2a10fd81ce4bfe09c9e190
assistantdev <106551222+assistantdev@users.noreply.github.com>
2022-05-30 14:35:08 -0300

    Sent by DeployFC at 30/05/2022 14:35:08

---
+++ htm/js/config.js
@@ -0,0 +1,260 @@
+/* Configurações gerais da loja */
+
+var oStoreConfig$={
+  "ModelID": [
+    3,
+    "Variável para diferenciar as imagens"
+  ],
+  "Product_Badges_Home_ProductList": [
+    true,
+    "Mostra badges na home e lista de produtos."
+  ],
+  "History_Page": [
+    true,
+    "Mostra histórico de páginas visitadas."
+  ],
+  "Product_SoldOut_Badge_ProductList": [
+    true,
+    "Mostra badge de produto esgotado na lista de produtos."
+  ],
+  "Product_SoldOut_Badge_SubProduct_ProductList": [
+    true,
+    "Mostra badge de subproduto esgotado na lista de produtos."
+  ],
+  "Change_Image_Hover_Home_ProductList": [
+    true,
+    "Ao passar o mouse, altera imagem do produto na home e lista de produtos."
+  ],
+  "Header_Voice_Search": [
+    true,
+    "Mostra busca por voz no campo de busca do topo."
+  ],
+  "News_Voice_Search": [
+    true,
+    "Mostra busca por voz na página de notícias."
+  ],
+  "Popup_Dont_Go": [
+    false,
+    "Mostra popup antes do visitante sair da página."
+  ],
+  "Product_List_Grid_Show": [
+    true,
+    "Mostra grade como produto na lista de produtos."
+  ],
+  "Product_List_Grid_Use_Main_Price": [
+    false,
+    "Mostra na grade da lista de produtos o preço do produto pai se subprodutos não tiverem preço."
+  ],
+  "Product_List_Grid_Soldout_Show": [
+    true,
+    "Mostra produtos esgotados na grade da lista de produtos."
+  ],
+  "Product_List_Grid_Single_Option": [
+    false,
+    "Mostra grade como produto na lista de produtos mesmo em produtos com única opção."
+  ],
+  "Product_List_Grid_FromPrice_Label_Show": [
+    true,
+    "Mostra legenda <b><i>A partir de</i></b> quando exibir na lista de produtos o menor preço da grade."
+  ],
+  "Product_List_Grid_Main_Descriptor_Label": [
+    "",
+    "Label do principal descritor de grade, que será adicionado ao nome do produto. ex: tam"
+  ],
+  "Product_List_Grid_Main_Descriptor_Field": [
+    "color",
+    "Nome do primeiro campo descritor de grade. Opções: color, desc1, desc2, desc3, descD1, descD2, descD3"
+  ],
+  "Product_Details_Show_Stock": [
+    false,
+    "Mostra estoque na página de detalhes dos produtos."
+  ],
+  "Product_Details_Show_Date_Promotion": [
+    false,
+    "Mostra data de término de promoção na página de detalhes dos produtos."
+  ],
+  "Home_Slideshow": [
+    true,
+    "Mostra banners rotativos na home."
+  ],
+  "Footer_Cookie_Policy": [
+    false,
+    "Mostra alerta política de cookies."
+  ],
+  "Footer_Payments": [
+    false,
+    "Mostra no rodapé as formas de pagamento aceitas pela loja."
+  ],
+  "Next_page_button": [
+    1,
+    "Exibe botão para autopaginação. Informe 0 para desabilitar ou digite a quantidade de páginas de produtos a partir da qual será exibido botão para ver mais produtos."
+  ],
+  "Show_wishlist": [
+    true,
+    "Exibe lista de desejos."
+  ],
+  "Recommend_Product": [
+    true,
+    "Mostra link na página de detalhes para recomendar o produto"
+  ],
+  "Grid_Pinch_Zoom": [
+    false,
+    "Efeito de pinça no zoom da grade"
+  ],
+  "Cart_ZipCode_Price_Side_Cart": [
+    true,
+    "Desabilitar valor zero do frete no carrinho lateral"
+  ],
+  "Cart_ZipCode_Price_Shopping_Cart": [
+    true,
+    "Desabilitar valor zero do frete no carrinho de compras"
+  ],
+  "Time_Slideshow": [
+    "5000",
+    "Tempo de transição entre banners."
+  ],
+  "Qty_Banner_Slideshow": [
+    "2",
+    "Mostra a quantidade de banners cadastrado no slideshow."
+  ],
+  "Show_Header_Menu": [
+    true,
+    "Mostra menu no topo"
+  ],
+  "Choose_Video_Player_Live_Store": [
+    "Twitch",
+    "Escolha entre Twitch e Youtube para Live Store"
+  ],
+  "Show_Video_Live_Store": [
+    "nomedocanal",
+    "Mostra vídeo da Live Store"
+  ],
+  "Facebook_Page_Link": [
+    "fastcommercebr",
+    "Para uso em link da ícone do rodapé para a página do Facebook da loja"
+  ],
+  "Twitter_Page_Link": [
+    "fastcommercebr",
+    "Para uso em link da ícone do rodapé para a página do Twitter da loja"
+  ],
+  "Instagram_Page_Link": [
+    "fastcommerce",
+    "Para uso em link da ícone do rodapé para a página do Instagram da loja"
+  ],
+  "Youtube_Page_Link": [
+    "fastcommercetv",
+    "Para uso em link da ícone do rodapé para a página do Youtube da loja"
+  ],
+  "Min_Home_Section_Prod": [
+    "0",
+    "0:Mostra seção mesmo sem produtos. 1:Mostra seção com pelo menos 1 produto. 2:Mostra seção com pelo menos 2 produtos. 3:Mostra seção com pelo menos 3 produtos. 4:Mostra seção com pelo menos 4 produtos"
+  ],
+  "Min_Home_Section_Prod_Special_Day": [
+    1,
+    "0:Mostra seção mesmo sem produtos. 1:Mostra seção com pelo menos 1 produto."
+  ],
+  "Use_Dynamic_Cart": [
+    false,
+    "Usa recursos do carrinho dinâmico"
+  ],
+  "Use_Prod_Skeleton": [
+    true,
+    "Mostra esqueleto antes de carregar conteúdo de produtos"
+  ],
+  "Show_WhatsApp": [
+    false,
+    "Exibe botão WhatsApp flutuante."
+  ],
+  "Phone_WhatsApp": [
+    "55119999988877",
+    "Informe telefone do WhatsApp. Não incluir traços, espaços, Parênteses e outros caracteres especiais."
+  ],
+  "UseProductDetailWindow": [
+    true,
+    "Exibe a Espiadinha 2.0"
+  ],
+  "Show_idCountryFC": [
+    false,
+    "Exibe o campo idCountryFC 'País' no checkout"
+  ],
+  "Show_idBirthdayFC": [
+    false,
+    "Exibe o campo idBirthdayFC 'Data nascimento' no checkout"
+  ],
+  "Show_idPassRemFC": [
+    false,
+    "Exibe o campo idPassRemFC 'Lembrete de senha' no checkout"
+  ],
+  "Show_idCellPhoneFC": [
+    false,
+    "Exibe o campo idCellPhoneFC 'Celular' no checkout"
+  ],
+  "Show_idGenderFC": [
+    false,
+    "Exibe o campo idGenderFC 'Sexo' no checkout"
+  ],
+  "Show_idAccInfoFC": [
+    false,
+    "Exibe título idAccInfoFC 'Dados para identificação' no checkout"
+  ],
+  "Show_idTxtCEP1FC": [
+    false,
+    "Exibe idTxtCEP1FC 'Clique se não souber CEP' no checkout"
+  ],
+  "Show_idLocalFC": [
+    true,
+    "Exibe o campo idLocalFC 'Local' em outro endereço de entrega do checkout"
+  ],
+  "Show_idParaFC": [
+    false,
+    "Exibe o campo idParaFC 'Para' em outro endereço de entrega do checkout"
+  ],
+  "Show_idPEmailFC": [
+    false,
+    "Exibe o campo idPEmailFC 'E-mail' em outro endereço de entrega do checkout"
+  ],
+  "Show_idPPaisFC": [
+    false,
+    "Exibe o campo idPPaisFC 'País' em outro endereço de entrega do checkout"
+  ],
+  "Show_idTxtCEP2FC": [
+    false,
+    "Exibe idTxtCEP2FC 'Clique se não souber CEP' em outro endereço de entrega do checkout"
+  ],
+  "Show_idTxtCPFFC": [
+    false,
+    "Para clientes B2C: Exibe texto idTxtCPFFC 'CPF opcional'"
+  ],
+  "Show_idOccupationFC": [
+    false,
+    "Para clientes B2C: Exibe o campo idOccupationFC 'Profissão' no checkout"
+  ],
+  "Show_idTxtCPFFC_PJ": [
+    false,
+    "Para clientes B2B: Exibe texto idTxtCPFFC 'CNPJ opcional'"
+  ],
+  "Show_idOccupationFC_PJ": [
+    false,
+    "Para clientes B2B: Exibe o campo idOccupationFC 'Ramo de Atuação' no checkout"
+  ],
+  "Show_idFAXFC_PJ": [
+    false,
+    "Para clientes B2B: Exibe o campo idFAXFC 'Fax' no checkout"
+  ],
+  "Show_TinyCheckout": [
+    true,
+    "Exibe checkout resumido após preenchimento"
+  ],
+  "Product_List_Progress_Bar_Show": [
+    true,
+    "Exibe barra de progresso na lista de produtos"
+  ],
+  "Carousel_Cross_Selling": [
+    true,
+    "Exibe carrossel no Cross Selling"
+  ],
+  "Carousel_History_Page": [
+    true,
+    "Exibe carrossel no Histórico"
+  ]
+};
